df4f455a965fc02c56d6f6cd7b771c1722f45c40
Rostislav Gogolauri 'mindfulme <alice.gogolauri@gmail.com>
2018-12-25 17:22:39 +0200

    python server

--- parser.py
+++ parser.py
@@ -3,10 +3,211 @@
 #class Parser:
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from flask import Flask
+app = Flask(__name__)
 
+class TreeNode:
+    def __init__(self,key,val,left=None,right=None,parent=None):
+        self.key = key
+        self.payload = val
+        self.leftChild = left
+        self.rightChild = right
+        self.parent = parent
+
+    def hasLeftChild(self):
+        return self.leftChild
+
+    def hasRightChild(self):
+        return self.rightChild
+
+    def isLeftChild(self):
+        return self.parent and self.parent.leftChild == self
+
+    def isRightChild(self):
+        return self.parent and self.parent.rightChild == self
+
+    def isRoot(self):
+        return not self.parent
+
+    def isLeaf(self):
+        return not (self.rightChild or self.leftChild)
+
+    def hasAnyChildren(self):
+        return self.rightChild or self.leftChild
+
+    def hasBothChildren(self):
+        return self.rightChild and self.leftChild
+
+    def replaceNodeData(self,key,value,lc,rc):
+        self.key = key
+        self.payload = value
+        self.leftChild = lc
+        self.rightChild = rc
+        if self.hasLeftChild():
+            self.leftChild.parent = self
+        if self.hasRightChild():
+            self.rightChild.parent = self
+
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def put(self,key,val):
+        if self.root:
+            self._put(key,val,self.root)
+        else:
+            self.root = TreeNode(key,val)
+        self.size = self.size + 1
+
+    def _put(self,key,val,currentNode):
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                   self._put(key,val,currentNode.leftChild)
+            else:
+                   currentNode.leftChild = TreeNode(key,val,parent=currentNode)
+        else:
+            if currentNode.hasRightChild():
+                   self._put(key,val,currentNode.rightChild)
+            else:
+                   currentNode.rightChild = TreeNode(key,val,parent=currentNode)
+
+    def __setitem__(self,k,v):
+       self.put(k,v)
+
+    def get(self,key):
+       if self.root:
+           res = self._get(key,self.root)
+           if res:
+                  return res.payload
+           else:
+                  return None
+       else:
+           return None
+
+    def _get(self,key,currentNode):
+       if not currentNode:
+           return None
+       elif currentNode.key == key:
+           return currentNode
+       elif key < currentNode.key:
+           return self._get(key,currentNode.leftChild)
+       else:
+           return self._get(key,currentNode.rightChild)
+
+    def __getitem__(self,key):
+       return self.get(key)
+
+    def __contains__(self,key):
+       if self._get(key,self.root):
+           return True
+       else:
+           return False
+
+    def delete(self,key):
+      if self.size > 1:
+         nodeToRemove = self._get(key,self.root)
+         if nodeToRemove:
+             self.remove(nodeToRemove)
+             self.size = self.size-1
+         else:
+             raise KeyError('Error, key not in tree')
+      elif self.size == 1 and self.root.key == key:
+         self.root = None
+         self.size = self.size - 1
+      else:
+         raise KeyError('Error, key not in tree')
+
+    def __delitem__(self,key):
+       self.delete(key)
+
+    def spliceOut(self):
+       if self.isLeaf():
+           if self.isLeftChild():
+                  self.parent.leftChild = None
+           else:
+                  self.parent.rightChild = None
+       elif self.hasAnyChildren():
+           if self.hasLeftChild():
+                  if self.isLeftChild():
+                     self.parent.leftChild = self.leftChild
+                  else:
+                     self.parent.rightChild = self.leftChild
+                  self.leftChild.parent = self.parent
+           else:
+                  if self.isLeftChild():
+                     self.parent.leftChild = self.rightChild
+                  else:
+                     self.parent.rightChild = self.rightChild
+                  self.rightChild.parent = self.parent
+
+    def findSuccessor(self):
+      succ = None
+      if self.hasRightChild():
+          succ = self.rightChild.findMin()
+      else:
+          if self.parent:
+                 if self.isLeftChild():
+                     succ = self.parent
+                 else:
+                     self.parent.rightChild = None
+                     succ = self.parent.findSuccessor()
+                     self.parent.rightChild = self
+      return succ
+
+    def findMin(self):
+      current = self
+      while current.hasLeftChild():
+          current = current.leftChild
+      return current
+
+    def remove(self,currentNode):
+         if currentNode.isLeaf(): #leaf
+           if currentNode == currentNode.parent.leftChild:
+               currentNode.parent.leftChild = None
+           else:
+               currentNode.parent.rightChild = None
+         elif currentNode.hasBothChildren(): #interior
+           succ = currentNode.findSuccessor()
+           succ.spliceOut()
+           currentNode.key = succ.key
+           currentNode.payload = succ.payload
+
+         else: # this node has one child
+           if currentNode.hasLeftChild():
+             if currentNode.isLeftChild():
+                 currentNode.leftChild.parent = currentNode.parent
+                 currentNode.parent.leftChild = currentNode.leftChild
+             elif currentNode.isRightChild():
+                 currentNode.leftChild.parent = currentNode.parent
+                 currentNode.parent.rightChild = currentNode.leftChild
+             else:
+                 currentNode.replaceNodeData(currentNode.leftChild.key,
+                                    currentNode.leftChild.payload,
+                                    currentNode.leftChild.leftChild,
+                                    currentNode.leftChild.rightChild)
+           else:
+             if currentNode.isLeftChild():
+                 currentNode.rightChild.parent = currentNode.parent
+                 currentNode.parent.leftChild = currentNode.rightChild
+             elif currentNode.isRightChild():
+                 currentNode.rightChild.parent = currentNode.parent
+                 currentNode.parent.rightChild = currentNode.rightChild
+             else:
+                 currentNode.replaceNodeData(currentNode.rightChild.key,
+                                    currentNode.rightChild.payload,
+                                    currentNode.rightChild.leftChild,
+                                    currentNode.rightChild.rightChild)
 
 class Stack:
-
 	def __init__(self):
 	    self.items = []
 	
@@ -16,7 +217,7 @@ class Stack:
 	def push(self, item):
 		self.items.insert(0,item)
 
-	def getTheFuckOutOfTheStack(self):
+	def pop(self):
 		return self.items.pop(0)
 
 	def peek(self):
@@ -25,60 +226,36 @@ class Stack:
 	def size(self):
 	    return len(self.items)
 	
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-class Solution(object):
-    # def sortedArrayToBST(self, nums):
-    #     """
-    #     :type nums: List[int]
-    #     :rtype: TreeNode
-    #     """
-    #     # Recursion with slicing
-    #     if not nums:
-    #         return None
-    #     mid = len(nums) / 2
-    #     root = TreeNode(nums[mid])
-    #     root.left = self.sortedArrayToBST(nums[:mid])
-    #     root.right = self.sortedArrayToBST(nums[mid + 1:])
-    #     return root
-
-    def sortedArrayToBST(self, divs):
-        # Recursion with index
-        return self.getHelper(nums, 0, len(nums) - 1)
-
-    def getHelper(self, nums, start, end):
-        if start > end:
-            return None
-        mid = (start + end) / 2
-        node = TreeNode(nums[mid])
-        node.left = self.getHelper(nums, start, mid - 1)
-        node.right = self.getHelper(nums, mid + 1, end)
-        return node
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == '__main__':
+    
+    html=urlopen("http://pythonscraping.com/pages/page1.html")
+    response = html.read()
 
-	html=urlopen("http://pythonscraping.com/pages/page1.html")
-	response = html.read()
+    html_soup = BeautifulSoup(response, 'html.parser')
+    divs = html_soup.find_all('div')
+    h1s = html_soup.find_all('h1')
 
-	html_soup = BeautifulSoup(response, 'html.parser')
-	divs = html_soup.find_all('div')
-	h1s = html_soup.find_all('h1')
+    type(html_soup)
 
-	type(html_soup)
+    s = Stack()
+    mytree = BinarySearchTree()
+    mytree[3]="red"
+    mytree[4]="blue"
+    mytree[6]="yellow"
+    mytree[2]="at"
 
-	s = Stack()
-	toTree = Solution()
-	
+    print(mytree[6])
+    print(mytree[2])
 
-	for x in divs:
-		s.push(x)
+    for x in divs:
+        s.push(x)
 
-	for y in h1s:
-		s.push(y)
+    for y in h1s:
+        s.push(y)
 
-	print(s.getTheFuckOutOfTheStack())
-	print(s.getTheFuckOutOfTheStack())
+    print(s.pop())
+    print(s.pop())
