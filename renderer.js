15be847165c9ac86758cbead19c5132ab264b20e
Rostislav Gogolauri 'mindfulme <alice.gogolauri@gmail.com>
2018-12-25 21:55:27 +0200

    parser

--- renderer.js
+++ renderer.js
@@ -2,27 +2,120 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 var exec = require('child_process').exec, child;
-var nodeConsole = require('console');
-var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 
+let divs = [];
 function parse() {
+    
     let url = document.getElementById("urlIn").value;
     console.log(url);
-    fetch(url)
-    .then((data) => { 
-        document.getElementById("result").value = JSON.stringify(data);
-     });
+    const options = {
+        uri: url,
+        transform: function (body) {
+          return cheerio.load(body);
+        }
+      };
+      
+      rp(options)
+      .then(($) => {
+        $('div').each(function(i, elem) {
+            divs.push($(this).text());
+        });
+        
+        console.log(divs);
+        document.getElementById("result").value = divs;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 }
 
 function serialize() {
-    let url = document.getElementById("urlIn").value;
-    myConsole.log(url);
-    fetch(url)
-    .then(data => data.json() )
-    .then((data) => { 
-        document.getElementById("result").value = data;
-     });
+    let val = document.getElementById("urlIn").value;
+    console.log(val);
+    data = val.split(' ');
+    divs = data;
+    document.getElementById("result").value = data;
+    
+}
+
+function search() {
+    let phrase = document.getElementById("popup1").value;
+    let resultOfSearch = divs.search(phrase);
+    let resultValue = "not found the phrase: " + phrase;
+    if(resultOfSearch)
+        resultValue = "found the phrase: " + phrase;
+        
+    document.getElementById("result").value = resultValue;
+}
+
+function filter() {
+    document.getElementById("result").value = "filtered by divs";
+}
+
+function sort() {
+    divs = divs.sort();
+    console.log(divs);
+    document.getElementById("result").value = divs;
 }
 
 document.querySelector('#parse').addEventListener('click', parse)
 document.querySelector('#serialize').addEventListener('click', serialize)
+
+document.querySelector('#search').addEventListener('click', search)
+document.querySelector('#filter').addEventListener('click', filter)
+document.querySelector('#sort').addEventListener('click', sort)
+
+
+// Get the modal
+var modal1 = document.getElementById('searchModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("search");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+    modal1.style.display = "none";
+    }
+}
+
+// Get the modal
+var modal2 = document.getElementById('filterModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("filter");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[1];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal2) {
+    modal2.style.display = "none";
+    }
+}
